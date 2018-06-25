ba83b659bfbaface9282f94be04eaeb561945241
Jessica Song <jiaqi.jessica.song@gmail.com>
2018-06-25 13:57:00 -0400

    Add files via upload

---
+++ AddLCLAdmin.vb
@@ -0,0 +1,25 @@
+﻿Imports System.DirectoryServices
+Imports System.DirectoryServices.AccountManagement
+
+Module AddLCLAdmin
+
+    Public localComp As String = System.Environment.MachineName
+
+    Sub Main(ByVal args() As String)
+        Dim GrpName As String = args(0)
+
+        Try
+            Dim localMachine As New DirectoryEntry("WinNT://" & localComp & ",computer")
+            Dim AdminGrp As DirectoryEntry = localMachine.Children.Find("Administrators", "group")
+
+            AdminGrp.Invoke("Add", New Object() {"WinNT://DOMAINNAME/" & GrpName})
+            AdminGrp.CommitChanges()
+
+            localMachine.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+End Module
